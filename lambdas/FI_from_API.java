46ead74aa5bb01a382b1a9b5deaeb0e29e805997
marciodev <marcio@marciopaulo.com.br>
2024-08-25 18:48:29 -0300

    functions, unary and binary operators

--- lambdas/FI_from_API.java
+++ lambdas/FI_from_API.java
@@ -7,10 +7,12 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class FI_from_API {
     public static void main(String[] args) {
@@ -19,6 +21,7 @@ public class FI_from_API {
         examples.supplier();
         examples.consumer();
         examples.function();
+        examples.unaryBinaryOperator();
     }
 
     public void predicate() {
@@ -65,16 +68,32 @@ public class FI_from_API {
         mapCapitalCities.forEach(mapPrint);
     }
 
-    public void function(){
+    public void function() {
+
+        //Function<T, R> is a functional interface i.e. one abstract method:
+        // R apply(T t)
 
         Function<String, Integer> fn2 = String::length;
         System.out.println("Function:" + fn2.apply("Moscow"));
 
-        BiFunction<String, String, Integer> biFn = (s1, s2)  -> s1.length() + s2.length();
-        System.out.println("BiFunction"+ biFn.apply("Willian ","Shakespeare"));
+        //Bifunctional<T,U,R> is a functional interface i.e one abstract method:
+        //  R apply(T t, U u)
+        BiFunction<String, String, Integer> biFn = (s1, s2) -> s1.length() + s2.length();
+        System.out.println("BiFunction" + biFn.apply("Willian ", "Shakespeare"));
 
-        BiFunction<String, String, String> biFn2 = (s1, s2)  -> s1.concat(s2);
-        System.out.println("BiFunction"+ biFn2.apply("Willian ","Shakespeare"));
+        BiFunction<String, String, String> biFn2 = (s1, s2) -> s1.concat(s2);
+        System.out.println("BiFunction" + biFn2.apply("Willian ", "Shakespeare"));
+
+    }
+
+    public void unaryBinaryOperator() {
+        // UnaryOperator<T> extends Function<T, T> is a functional interface i.e. one abstract method:
+        // T apply(T t)
+        UnaryOperator<String> unaryOp = name -> "My name is " + name;
+        System.out.println("Unary operator :" + unaryOp.apply("Marcio"));
+
+        BinaryOperator<String> binaryOp = (s1, s2) -> s1.concat(s2);
+        System.out.println("Binary operator" + binaryOp.apply("Marcio ", "Paulo"));
 
     }
 }
