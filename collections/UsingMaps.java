6d44dd6d70b1d6d51ae058d7f62470db7ddf9dc3
marciodev <marcio@marciopaulo.com.br>
2024-12-07 20:19:12 -0300

    maps

--- collections/UsingMaps.java
+++ collections/UsingMaps.java
@@ -5,6 +5,7 @@ import models.User;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Set;
 
 public class UsingMaps {
 
@@ -26,6 +27,34 @@ public class UsingMaps {
         for (Integer age : map.values()) {
             System.out.println(age);
         }
+
+        //foreach(BiConsumer)
+        //Biconsumer<T,U> void accept(T t, U u);
+        map.forEach((k, v) -> System.out.println(k + " maps to " + v));
+
+        //Set<Map.Entry<k,V> entrySet() - Map.Entry encapsulates a key-value pair.
+        //go from a Map to a set (an official collection)
+
+        map.entrySet().forEach( entry -> System.out.println(entry.getKey() + " --- "+ entry.getValue()));
+
+        Set<String> keys = map.keySet();
+
+        //PutIfAbsent -> put if value is null.
+        map.put("Mike", null); //{Chris=33, John=18, Mary=21, Mike=null}
+        map.putIfAbsent("Chris", 99); //Chris continue with 33
+        map.putIfAbsent("Mike", 10);
+        System.out.println(map); //{Chris=33, John=18, Mary=21, Mike=10}
+
+        //replace() and replaceAll
+        Integer original = map.replace("Chris", 81);
+        System.out.println(map); //{Chris=81, John=18, Mary=21, Mike=10}
+        //BiFunction<T, U, R>
+        //R apply(T t, U u) - 2 inputs and an output; all of which can be different types
+        //replaceAll(BiFunction<k,V,V> fn) - note the return type is of type V also
+        map.replaceAll((name,age) -> name.length()); //{Chris=5, John=4, Mary=4, Mike=4}
+        System.out.println(map);
+
+        map.remove("Mike");
 
         otherExample();
     }
